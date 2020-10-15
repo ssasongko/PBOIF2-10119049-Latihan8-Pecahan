9d249e24fabd4f94c4933b0527f64317f69fc4fc
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:43:08 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan8-Pecahan/src/pboif2/pkg10119049/latihan8/pecahan/PBOIF210119049Latihan8Pecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan8.pecahan;
+
+/**
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program untuk menampilkan angka pecahan
+ */
+public class PBOIF210119049Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float  : " + f);
+        System.out.println("Double : " + d);
+        
+    }
+    
+}
